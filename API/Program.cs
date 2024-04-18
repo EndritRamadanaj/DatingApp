7feb05e40341d23c303ee8b712fb065aeefc444c
Endrit <endritramadanaj@hotmail.com>
2024-04-18 15:21:48 +0200

    builded the client side with Angular

--- API/Program.cs
+++ API/Program.cs
@@ -10,12 +10,10 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+builder.Services.AddCors();
 var app = builder.Build();
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
